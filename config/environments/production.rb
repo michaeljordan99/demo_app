9a338b475c5dda05222eaff3a5cc304b69e6e4c5
michaeljordan99 <michaeljordan99@hotmail.com>
2012-01-15 19:57:35 -0800

    Updated because of issue while deploying to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ DemoApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #MJ 1/15/2012 changed this from false to true because of an error while deploying to Heroku.
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
